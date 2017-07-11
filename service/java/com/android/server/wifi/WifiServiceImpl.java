dfcbf9f05387a34a86c413fd0e4fd0d4ded8ac94
Abhishek Srivastava <cabsriv@codeaurora.org>
2017-07-11 11:37:08 +0530

    SoftAp: Refactor SoftAP client notification logic
    This commit seprates Notification feature to a new class
    WifiSapApNotificationManager. It exposes an API getConnectedStations()
    through which application can query number of connected clients to
    softap at any instant.
    
    Change-Id: I9c8f6eab1ae1808907845b594bcdeb3655d541c0

--- service/java/com/android/server/wifi/WifiServiceImpl.java
+++ service/java/com/android/server/wifi/WifiServiceImpl.java
@@ -130,6 +130,10 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.Collections;
+import android.net.wifi.WifiDevice;
+import com.android.server.wifi.WifiSoftApNotificationManager;
+
 
 /**
  * WifiService handles remote WiFi operation requests by implementing
@@ -2780,6 +2784,13 @@ public class WifiServiceImpl extends IWifiManager.Stub {
                         supplicantData, ipConfigData);
         restoreNetworks(wifiConfigurations);
         Slog.d(TAG, "Restored supplicant backup data");
+    }
+    public List<WifiDevice> getConnectedStations() {
+        if (mContext.getResources().getBoolean(com.android.internal.R.bool.config_softap_extension)) {
+            return WifiSoftApNotificationManager.getInstance(mContext).getConnectedStations();
+        } else {
+            return Collections.emptyList();
+        }
     }
 
 }
